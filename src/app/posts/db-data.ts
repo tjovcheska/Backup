f84d528d94cb6ecc3a77c34b871174d87f889a32
Teodora <jovcheskateodora@gmail.com>
2019-12-28 21:54:16 +0100

    Description

---
+++ src/app/posts/db-data.ts
@@ -0,0 +1,63 @@
+export const POSTS: any=[
+    undefined,
+    {
+        id:1,
+        description: "Description1"
+    },
+    {
+        id:2,
+        description: "Description2"
+    },
+    {
+        id:3,
+        description: "Description3"
+    },
+    {
+        id:4,
+        description: "Description4"
+    },
+    {
+        id:5,
+        description: "Description5"
+    },
+    {
+        id:6,
+        description: "Description6"
+    },
+    {
+        id:7,
+        description: "Description7"
+    },
+    {
+        id:8,
+        description: "Description8"
+    },
+    {
+        id:9,
+        description: "Description9"
+    },
+    {
+        id:10,
+        description: "Description10"
+    },
+    {
+        id:11,
+        description: "Description11"
+    },
+    {
+        id:12,
+        description: "Description12"
+    },
+    {
+        id:13,
+        description: "Description13"
+    },
+    {
+        id:14,
+        description: "Description14"
+    },
+    {
+        id:15,
+        description: "Description15"
+    }
+];
